9451e42566a68cae6e1ccba98154228cb77aecef
moh-sarwat <m.s.albehery@gmail.com>
2025-03-13 10:41:58 +0000

    Debugging Railway crash - running Flask without Gunicorn

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def scan():
     return jsonify(result)
 
 # Use Railway’s dynamic PORT environment variable
+# if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 5000))
+#    app.run(host="0.0.0.0", port=port)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
